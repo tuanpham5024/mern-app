39aac516bea0483da63a795d52e1d58810f9086b
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-09-14 01:36:47 +0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+
+const authRouter = require('./routes/auth');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-app.vlu4w.mongodb.net/mern-app?retryWrites=true&w=majority`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+connectDB();
+
+const app = express();
+
+app.use('/api/auth', authRouter)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, (req, res) => console.log(`Server started on port ${PORT}`));
